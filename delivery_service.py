a611de19b295d751d1e0edf9bc6421b21234c6e1
4udo <4udodeev@gmail.com>
2023-12-27 22:16:56 +0300

    правки после первого ревью

--- delivery_service.py
+++ delivery_service.py
@@ -1,43 +1,25 @@
 # ID успешной посылки - 103960421
 
-class main():
-    weights_list: list = list(map(int, input().split()))
-    limit: int = int(input())
+def calculate_platforms(weights: list[int], limit: int) -> int:
+    """Функция расчета оптимального количества платформ"""
+    platforms: int = 0
+    sorted_weights: list = sorted(weights)
+    left_index: int = 0
+    right_index: int = len(sorted_weights)-1
 
-    def calculate_platforms(weights: list[int], limit: int) -> int:
-        """Функция расчета оптимального количества платформ"""
-        platforms: int = 0
+    while left_index <= right_index:
 
-        correct_weights: list = [elem for elem in weights if elem <= limit]
-        correct_weights.sort()
-        if len(correct_weights) == 0:
-            return 0
-        if len(correct_weights) == 1 and correct_weights[0] <= limit:
-            return 1
+        if (sorted_weights[left_index] + sorted_weights[right_index]
+                <= limit):
+            left_index += 1
 
-        left_index: int = 0
-        right_index: int = len(correct_weights)-1
+        platforms += 1
+        right_index -= 1
 
-        while left_index < right_index:
-            if correct_weights[right_index] == limit:
-                platforms += 1
-                right_index -= 1
-            elif (correct_weights[left_index] + correct_weights[right_index]
-                  <= limit):
-                platforms += 1
-                right_index -= 1
-                left_index += 1
-            else:
-                platforms += 1
-                right_index -= 1
-            if (left_index == right_index
-               and correct_weights[left_index] <= limit):
-                platforms += 1
-
-        return platforms
-
-    print(calculate_platforms(weights_list, limit))
+    return platforms
 
 
-if __name__ == '__main__':
-    main()
+weights_list: list = [int(elem) for elem in input().split()]
+limit: int = int(input())
+
+print(calculate_platforms(weights_list, limit))
